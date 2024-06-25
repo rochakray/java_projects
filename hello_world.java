f20e0d255084359b1dbd5c8510543e952ccaa853
rochakray <rochak.ray@gmail.com>
2024-06-24 20:31:21 -0400

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -2,5 +2,6 @@ class hello_world
 {
     public static void main(String[] args) {
         System.out.println("Hello World , Good Evening");
+        System.out.println("Remote repo in Sweden");
     }
 }
